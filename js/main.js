2611218770fc9344c740e04abc7d58f9973522aa
Thiendeptrai12 <94940067+Thiendeptrai12@users.noreply.github.com>
2022-06-02 20:06:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function firstQuestion(){
         text: CONFIG.introDesc,
 
         // 2. Ảnh ở câu hỏi đầu tiên
-        imageUrl: 'img/cuteCat.jpg',
+        imageUrl: 'img/280888908_7425706804171327_8614893030963856737_n.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
