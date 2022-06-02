f2bcbaf10fbbbe852138964b1f9cf2a2139321f2
Thiendeptrai12 <94940067+Thiendeptrai12@users.noreply.github.com>
2022-06-02 20:09:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: 'chào thằng hiếu béo',
+    introTitle: 'chào bạn:)',
     introDesc: `Thủ tướng thiện đz có điều này muốn nói`,
     btnIntro: 'Sủa lẹ',
     title: 'Có một sự thật là thằng hiếu bạn tôi bị đần FA 16 năm nay và tao muốn biết là thằng hiếu có đẹp trai không ? 😙',
     desc: 'tao cần tìm ny cho nó ngay lập tức đứa nào ứng không :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnYes: 'Eo ơi thằng hiếu đẹp trai hết phần thiên hạ kìa',
     btnNo: 'Đm thằng hiếu xấu vãi l 💔 ',
     question:'Anh hiếu đz em mê vcl làm ny em nhé anh hiếu:33',
     reasonPlaceholder: 'Lí Do',
@@ -13,6 +13,6 @@ const CONFIG = {
     reply: 'Tại vì thằng hiếu được thừa hưởng sự đz từ thằng thiện =))',
     mess: 'Tao biết mà kkk làm ny của hiếu đi nhaaa :333',
     messDesc: '💬💬 Nhắn tin cho Anh hiếu nà :33 💬💬',
-    btnAccept: 'Ok lun 💖',
+    btnAccept: 'Dm ok luôn',
     messLink: 'https://www.facebook.com/messages/t/100066541512538'
 }
