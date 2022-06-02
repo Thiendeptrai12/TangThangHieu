c6d518f2d2f3d4781f04cc9255c32d1571e2f422
Thiendeptrai12 <94940067+Thiendeptrai12@users.noreply.github.com>
2022-06-02 19:59:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    introTitle: 'chào thằng hiếu béo',
+    introDesc: `Thủ tướng thiện đz có điều này muốn nói`,
+    btnIntro: 'Sủa lẹ',
+    title: 'Có một sự thật là thằng hiếu bạn tôi bị đần FA 16 năm nay và tao muốn biết là thằng hiếu có đẹp trai không ? 😙',
+    desc: 'tao cần tìm ny cho nó ngay lập tức đứa nào ứng không :> ',
     btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
-    reasonPlaceholder: 'Lý do',
-    btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnNo: 'Đm thằng hiếu xấu vãi l 💔 ',
+    question:'Anh hiếu đz em mê vcl làm ny em nhé anh hiếu:33',
+    reasonPlaceholder: 'Lí Do',
+    btnReply: 'Hãy cho biết vì sao thằng hiếu đz 💌💌',
+    reply: 'Tại vì thằng hiếu được thừa hưởng sự đz từ thằng thiện =))',
+    mess: 'Tao biết mà kkk làm ny của hiếu đi nhaaa :333',
+    messDesc: '💬💬 Nhắn tin cho Anh hiếu nà :33 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://www.facebook.com/messages/t/100066541512538'
 }
